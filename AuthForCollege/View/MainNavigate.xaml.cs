72ba513c3367e55947fc3c2dff284da73e795fc7
Drenuv <iwanov.timofei2001@yandex.ru>
2020-11-18 14:49:24 +0300

    NavigationWindow

---
+++ AuthForCollege/View/MainNavigate.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AuthForCollege.View
+{
+    /// <summary>
+    /// Логика взаимодействия для MainNavigate.xaml
+    /// </summary>
+    public partial class MainNavigate : NavigationWindow
+    {
+        public MainNavigate()
+        {
+            InitializeComponent();
+            this.Content = new View.ListTeachers();
+            this.Navigate(new View.ListTeachers());
+            
+        }
+    }
+}
